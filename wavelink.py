a47f2a7f70867aaae86e2c27290d84a98e207ac6
Robin Bonny <robin.bonny@epfl.ch>
2023-08-08 17:15:26 +0200

    Fixes

--- wavelink.py
+++ wavelink.py
@@ -2,12 +2,13 @@ import sys
 import re
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QDoubleSpinBox, QPushButton, QGridLayout, QComboBox, QLineEdit, QWidget, QSpinBox, QTextEdit
 
+
 class DataInputApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('PyWavelink')
         self.setGeometry(100, 100, 400, 500)  # Adjust window dimensions
-        
+
         self.init_ui()
 
     def init_ui(self):
@@ -16,10 +17,12 @@ class DataInputApp(QMainWindow):
 
         layout = QGridLayout()
 
-        saved_value_options = ['Select', 'WR-75', 'WR-51', 'WR-42', 'WR-34', 'WR-28', 'Coax']
+        saved_value_options = ['Select', 'WR-75',
+                               'WR-51', 'WR-42', 'WR-34', 'WR-28', 'Coax']
         self.saved_values_combo = QComboBox()
         self.saved_values_combo.addItems(saved_value_options)
-        self.saved_values_combo.currentIndexChanged.connect(self.handle_saved_value_selection)
+        self.saved_values_combo.currentIndexChanged.connect(
+            self.handle_saved_value_selection)
 
         layout.addWidget(QLabel("Saved Value:"), 0, 0)
         layout.addWidget(self.saved_values_combo, 0, 1, 1, 2)
@@ -39,7 +42,8 @@ class DataInputApp(QMainWindow):
             "Power (dBm)",
             "Calibration"
         ]
-        self.spin_boxes = [QDoubleSpinBox() if label != "Power (dBm)" else QSpinBox() for label in input_labels]
+        self.spin_boxes = [QDoubleSpinBox() if label not in (
+            "Points", "Power (dBm)") else QSpinBox() for label in input_labels]
         self.calibration_edit = QLineEdit()
         self.calibration_edit.setPlaceholderText('Enter calibration data')
 
@@ -47,7 +51,6 @@ class DataInputApp(QMainWindow):
         for idx, (label, spin_box, default_value) in enumerate(zip(input_labels, self.spin_boxes, default_values)):
             layout.addWidget(QLabel(label), idx + 2, 0)
             if label == "Points":
-                spin_box = QSpinBox()
                 spin_box.setMinimum(1)
                 spin_box.setMaximum(100001)
             elif label == "Power (dBm)":
@@ -68,25 +71,42 @@ class DataInputApp(QMainWindow):
 
         self.saved_value_data = {
             'WR-75': [9.9, 15.0, 1021, 1, -10],
-            'WR-51': [14.5, 22.0, 1501, 1, -10],
-            'WR-42': [17.6, 26.7, 1821, 1, -10],
-            'WR-34': [21.7, 33.0, 2261, 1, -10],
-            'WR-28': [26.3, 40.0, 2741, 1, -10],
+            'WR-51': [14.5, 22.0, 1501, 2, -12],
+            'WR-42': [17.6, 26.7, 1821, 4, -15],
+            'WR-34': [21.7, 33.0, 2261, 5, -16],
+            'WR-28': [26.3, 40.0, 2741, 6, -17],
             'Coax': [2.0, 6.0, 1001, 1, -10]
+        }
+
+        self.calibration_data = {
+            'WR-75': 'WR75',
+            'WR-51': 'WR51',
+            'WR-42': 'WR42',
+            'WR-34': 'WR34',
+            'WR-28': 'WR28',
+            'Coax': 'coaxcav'
         }
 
     def handle_saved_value_selection(self, index):
         selected_value = self.saved_values_combo.currentText()
         if selected_value in self.saved_value_data:
             values = self.saved_value_data[selected_value]
-            for spin_box, value in zip(self.spin_boxes, values):
+            for idx, (spin_box, value) in enumerate(zip(self.spin_boxes, values)):
                 spin_box.setValue(value)
+
+            # Update the calibration text field if a calibration value exists for the selected option
+            if selected_value in self.calibration_data:
+                self.calibration_edit.setText(
+                    self.calibration_data[selected_value])
+            else:
+                # Clear the text field if no calibration value is available
+                self.calibration_edit.clear()
 
     def handle_submit(self):
         input_values = [spin_box.value() for spin_box in self.spin_boxes]
         ip_address = self.ip_address_edit.text()
         calibration_data = self.calibration_edit.text()
-        
+
         if self.validate_ipv4(ip_address):
             # Do something with the input values, valid IP address, and calibration data
             print("Input Values:", input_values)
@@ -99,11 +119,13 @@ class DataInputApp(QMainWindow):
         ipv4_pattern = re.compile(r"^(?:\d{1,3}\.){3}\d{1,3}$")
         return ipv4_pattern.match(ip_address) is not None
 
+
 def main():
     app = QApplication(sys.argv)
     window = DataInputApp()
     window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
